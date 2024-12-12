53ee1161912fa0b5e3ee60dfa6aafcea34491408
ed-chick <edward.chick@nhs.net>
2024-12-12 09:06:14 +0000

    INIT: adding the essential content

---
+++ test_app.py
@@ -0,0 +1,20 @@
+# test app
+
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+st.set_page_config(
+        page_title="Test app", page_icon=":chart_with_upwards_trend:",
+        layout='wide'
+    )
+
+def main():
+    st.title("Test")
+
+if __name__ == "__main__":
+
+    main()
